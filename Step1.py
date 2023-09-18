de7325211de2a198a0d3af0dfcbe97fd8f8012ef
Hoang Ha <64120343+hahuyhoang411@users.noreply.github.com>
2023-09-18 23:55:01 +0700

    Add files via upload

---
+++ Step1.py
@@ -0,0 +1,23 @@
+# Install streamlit
+# pip install streamlit
+
+import streamlit as st
+
+st.title("[VietAI-NTI] Chatbot")
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# Accept user input
+if prompt := st.chat_input("What is up?"):
+    # Display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown(prompt)
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
